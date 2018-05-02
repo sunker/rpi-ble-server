f5315f62adae02999ca5ab91ab1e278bad54e1c9
Erik Sundell <erik.sundell87@gmail.com>
2018-05-02 20:37:23 +0200

    Ref

--- GpsCharacteristic.js
+++ GpsCharacteristic.js
@@ -11,9 +11,7 @@ module.exports = class GpsCharacteristic extends bleno.Characteristic {
       value: null
     })
     this.previousSpeed = null
-    gpsdClient.on('coordinate', (coord) => {
-      this.sendNotification(coord)
-    })
+    gpsdClient.on('coordinate', this.sendNotification)
   }
 
   onSubscribe (maxValueSize, updateValueCallback) {
