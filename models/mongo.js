fbf673c0b19c6292b5846880983f0325f2ef218e
Erik Sundell <erik.sundell87@gmail.com>
2018-05-04 22:46:23 +0200

    Ref

--- models/mongo.js
+++ models/mongo.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 
-const url = 'mongodb://localhost:27017/'
+const url = 'mongodb://localhost:27017/annika'
 
 var mongo = {
   db: null,
@@ -9,10 +9,9 @@ var mongo = {
 
   connect: function () {
 
-    MongoClient.connect(url, function (err, client) {
+    MongoClient.connect(url, function (err, db) {
       console.log('err', err)
       console.log("Connected successfully to mongodb")
-      const db = client.db('annika');
       mongo.db = db
       mongo.coordinates = db.collection('documents')
     })
