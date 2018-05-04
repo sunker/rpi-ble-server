25f5107177e53018e31920d719836c098b19a6f5
Erik Sundell <erik.sundell87@gmail.com>
2018-05-04 22:24:59 +0200

    Ref

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const GpsdClient = require('./GpsdClient')
 const blenoHub = require('./blenoHub')
+const gpsWorker = require('./GpsWorker')
 
 const gpsdClient = new GpsdClient()
 blenoHub.init(gpsdClient)
 
+gpsWorker(gpsdClient)
