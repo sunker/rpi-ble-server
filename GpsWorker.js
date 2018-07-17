7907e358d2627178c5f9830d12be9641b6fc3929
Erik Sundell <erik.sundell87@gmail.com>
2018-07-17 12:39:24 +0200

    ref

--- GpsWorker.js
+++ GpsWorker.js
@@ -53,6 +53,8 @@ module.exports = class GpsdWorker extends EventEmitter {
       const { latitude, longitude } = doc
       doc.distance = this.previousCoordinate ? distance(this.previousCoordinate.latitude, this.previousCoordinate.longitude, latitude, longitude).toFixed(8) : 0
       console.log('doc.distance', doc.distance)
+      console.log('this.previousCoordinate', this.previousCoordinate)
+      console.log('this.previousCoordinate.totalDistance', this.previousCoordinate.totalDistance)
       doc.totalDistance = (this.previousCoordinate && this.previousCoordinate.totalDistance ? (Number(this.previousCoordinate.totalDistance) + Number(doc.distance)) : Number(doc.distance)).toFixed(6)
       console.log('Total distance: ', doc.totalDistance)
       // if (doc.speed > 0.1) {
