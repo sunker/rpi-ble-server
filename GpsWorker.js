a1b693a88f729f770dcd0b3551987a43b230a6e4
Erik Sundell <erik.sundell87@gmail.com>
2018-07-17 12:44:05 +0200

    ref

--- GpsWorker.js
+++ GpsWorker.js
@@ -43,27 +43,31 @@ module.exports = class GpsdWorker extends EventEmitter {
         if (err) console.log('error at mongo insert telemetry', err)
         else console.log('Stored in db. Speed', doc.speed)
       })
-    } 
+    }
   }
 
   aggregate () {
-    if (this.coordinates.length > 0) {
-      const doc = this.coordinates[this.coordinates.length - 1]
-      doc.speed = this.getAverageSpeed()
-      const { latitude, longitude } = doc
-      doc.distance = this.previousCoordinate ? distance(this.previousCoordinate.latitude, this.previousCoordinate.longitude, latitude, longitude).toFixed(8) : 0
-      console.log('this', this)
-      console.log('doc.distance', doc.distance)
-      console.log('this.previousCoordinate', this.previousCoordinate)
-      console.log('this.previousCoordinate.totalDistance', this.previousCoordinate.totalDistance)
-      doc.totalDistance = (this.previousCoordinate && this.previousCoordinate.totalDistance ? (Number(this.previousCoordinate.totalDistance) + Number(doc.distance)) : Number(doc.distance)).toFixed(6)
-      console.log('Total distance: ', doc.totalDistance)
-      // if (doc.speed > 0.1) {
-      this.storeCoordinate(doc)
-      this.emit('totalDistance', doc.totalDistance)
-      // }
-      this.previousCoordinate = doc
-      this.coordinates = []
+    try {
+      if (this.coordinates.length > 0) {
+        const doc = this.coordinates[this.coordinates.length - 1]
+        doc.speed = this.getAverageSpeed()
+        const { latitude, longitude } = doc
+        doc.distance = this.previousCoordinate ? distance(this.previousCoordinate.latitude, this.previousCoordinate.longitude, latitude, longitude).toFixed(8) : 0
+        console.log('this', this)
+        console.log('doc.distance', doc.distance)
+        console.log('this.previousCoordinate', this.previousCoordinate)
+        console.log('this.previousCoordinate.totalDistance', this.previousCoordinate ? this.previousCoordinate.totalDistance : 'röv')
+        doc.totalDistance = (this.previousCoordinate && this.previousCoordinate.totalDistance ? (Number(this.previousCoordinate.totalDistance) + Number(doc.distance)) : Number(doc.distance)).toFixed(6)
+        console.log('Total distance: ', doc.totalDistance)
+        // if (doc.speed > 0.1) {
+        this.storeCoordinate(doc)
+        this.emit('totalDistance', doc.totalDistance)
+        // }
+        this.previousCoordinate = doc
+        this.coordinates = []
+      }
+    } catch (error) {
+      console.log('error', error)
     }
   }
 }
